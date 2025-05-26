637f9e2606b69f4b553c81f68328a3a4cadbe96d
tamannashah18 <tamannashah1809@gmail.com>
2025-05-26 11:25:19 +0530

    changed api/upload-image/route.js

--- src/app/api/upload-image/route.js
+++ src/app/api/upload-image/route.js
@@ -6,7 +6,7 @@ export const config = { api: { bodyParser: false } };
 export async function POST(request) {
   try {
     const { searchParams } = new URL(request.url);
-    const filename = searchParams.get('filename') || `image-${Date.now()}`;
+    const filename = searchParams.get('filename') +`${Date.now()}`;
 
     // The request body is the file
     const blob = await put(filename, request.body, { access: 'public' });
